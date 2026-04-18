ac4d41aa334be0e329a9b679112fe91b2b4c811f
deepaksenhacker <care.deepaksen@gmail.com>
2026-04-18 18:20:36 +0530

    remove extra function call

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -2,9 +2,6 @@ import mongoose from "mongoose";
 import 'dotenv/config';
 
 
-dotenv.config();
-
-
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
